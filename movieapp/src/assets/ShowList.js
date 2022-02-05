e7ad3a3e1b2cfa88463f4a4077cf98049e8332ca
Dmitry Shevtsov <dshevtsov20@gmail.com>
2022-02-05 15:03:03 +0300

    ref: add assets

--- movieapp/src/assets/ShowList.js
+++ movieapp/src/assets/ShowList.js
@@ -4,55 +4,55 @@ export const shows = [
     title: "Чёрная вдова",
     rating: "6.8",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1.png",
   },
   {
     id: 2,
     title: "Шан-чи",
     rating: "7.9",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-1.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-1.png",
   },
   {
     id: 3,
     title: "Локи",
     rating: "8.4",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-2.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-2.png",
   },
   {
     id: 4,
     title: "Как я встретил вашу маму",
     rating: "8.3",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-3.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-3.png",
   },
   {
     id: 5,
     title: "Бумажный дом",
     rating: "8.3",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-4.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-4.png",
   },
   {
     id: 6,
     title: "Друзья",
     rating: "8.8",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-5.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-5.png",
   },
   {
     id: 7,
     title: "Теория большого взрыв",
     rating: "8.1",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-6.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-6.png",
   },
   {
     id: 8,
     title: "Два с половиной человека",
     rating: "7",
     image:
-      "/films/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-7.png",
+      "images/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1-7.png",
   },
 ];
