05b62a908302e586ca77ef01d9f4acdc4832ff61
Dmitry Shevtsov <dshevtsov20@gmail.com>
2022-02-09 12:07:06 +0300

    feat: add loading icon

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -17,7 +17,7 @@ const Wrapper = styled.div`
 `;
 const Container = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   background-color: #121829;
   background-image: url(${bgImage});
   background-position: top;
--- movieapp/src/components/view/View.jsx
+++ movieapp/src/components/view/View.jsx
@@ -2,10 +2,25 @@ import React, { useEffect, useState } from "react";
 import Title from "./title/Title";
 import Content from "./content/Content";
 import styled from "styled-components";
+import load from "../../assets/loading.svg";
 
 const StyledView = styled.section`
   margin-top: 64px;
   margin-bottom: 156px;
+`;
+
+const Loader = styled.div`
+  width: 100%;
+  min-height: 100%;
+  margin-top: 200px;
+  margin-bottom: 24px;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledImg = styled.img`
+  width: 56px;
+  height: 56px;
 `;
 
 function View(props) {
@@ -24,12 +39,11 @@ function View(props) {
   useEffect(() => {
     window.scrollTo(0, 0);
     window.sessionStorage.setItem("page", page);
+    setData(null);
     fetch(`https://api.themoviedb.org/3/${props.code}/popular?api_key=${process.env.REACT_APP_API_CODE}&language=ru-RU&page=${page}`)
-    .then(res => {
-      res.json()
-      .then(result => {
-        setData(result);
-      })
+    .then(res => res.json() )
+    .then(result => {
+      setData(result);
     })
   }, [ props.code, page]);
 
@@ -43,7 +57,11 @@ function View(props) {
   return (
     <StyledView>
       <Title title={props.page} />
-      {data === null ? <div>+</div> : <Content data={data} name={props.code === "tv" ? "name" : "title"} inc={incPage} dec={decPage} />}
+        {data === null ? 
+        <Loader>
+          <StyledImg src={load} alt="Загрузка"/>
+        </Loader> 
+        : <Content data={data} name={props.code === "tv" ? "name" : "title"} inc={incPage} dec={decPage} />}
     </StyledView>
   );
 }
